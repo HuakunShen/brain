12b91fe6f9e2b298e9561f02c2b218118b65494e
Huakun Shen <huakun.shen@huakunshen.com>
2022-12-29 03:04:10 -0500

    Add 5 more submodules

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,14 +10,14 @@ const config = {
   tagline: 'Dinosaurs are cool',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'HuakunShen', // Usually your GitHub org/user name.
+  projectName: "Huakun's Brain", // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
