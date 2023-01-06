b36afa45412347e1084addd71defcee5049471d9
Huakun Shen <huakun.shen@huakunshen.com>
2023-01-06 18:28:01 -0500

    Add update-all.sh for submodule update

--- update-all.sh
+++ update-all.sh
@@ -6,10 +6,7 @@ submodulePaths=$(git submodule | awk '{$1=$1;print}' | cut -d ' ' -f 2)
 
 function git_add_commit_update {
     nothing=$(git status | grep 'nothing to commit')
-    echo nothing $nothing
     if [[ -z $nothing ]]; then
-        echo "nothing to commit for $PWD"
-    else
         git add .
         git commit -m "Update Module"
         git push
@@ -20,6 +17,7 @@ for path in $submodulePaths
 do
     cd $path
     detached_head=$(git branch | grep 'HEAD detached')
+    echo $path
     if [[ -z $detached_head ]]; then
         git_add_commit_update
     else
@@ -27,8 +25,6 @@ do
         git checkout master
         git stash pop
         git_add_commit_update
-        # echo "Detached" $path
-        # echo "Warn: have to be manually fixed"
     fi
     cd $cwd
 done
